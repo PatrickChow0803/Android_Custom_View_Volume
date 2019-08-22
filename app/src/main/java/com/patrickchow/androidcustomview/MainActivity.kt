1c3e330bff1267a3f8fe35a743288f5c6ec4f45b
Patrick Chow <PatrickChow0803@gmail.com>
2019-08-22 19:14:28 -0400

    The red circle is now movable

--- app/src/main/java/com/patrickchow/androidcustomview/MainActivity.kt
+++ app/src/main/java/com/patrickchow/androidcustomview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.patrickchow.androidcustomview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.patrickchow.androidcustomview.view.CircleCustomView
 
 class MainActivity : AppCompatActivity() {
     
@@ -9,5 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
--- app/src/main/java/com/patrickchow/androidcustomview/view/CircleCustomView.kt
+++ app/src/main/java/com/patrickchow/androidcustomview/view/CircleCustomView.kt
@@ -7,12 +7,54 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.Toast
+import com.patrickchow.androidcustomview.MainActivity
 
 
 class CircleCustomView (context: Context?, attrs: AttributeSet?): View(context, attrs){
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var toast = Toast(context)
+
+    var canvasRotation = rotation
+    
+    private var xStart: Float = 0f
+    private var yStart: Float = 0f
+    private var xDiff: Float = 0f
+    private var yDiff: Float = 0f
+    var volume = 0
+
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when(event?.action){
+            //Is called when the user pushes on the view
+            MotionEvent.ACTION_DOWN -> {
+                Log.i("hello", "world")
+                xStart = event.x
+            }
+            //Is called when the user drags their finger across
+            MotionEvent.ACTION_MOVE ->{
+                Log.i("You Moved", "world")
+                xDiff = event.x - xStart
+                rotation = canvasRotation + xDiff/10
+            }
+            //Is called when the user lifts up their finger
+
+            MotionEvent.ACTION_UP ->{
+                volume = (rotation/360*100).toInt()
+                Log.i("Goodbye", "world")
+                toast = Toast.makeText(context, "Volume Level is: $volume", Toast.LENGTH_SHORT)
+                toast.show()
+                canvasRotation = rotation
+            }
+
+        }
+
+        return true
+    }
+
     override fun onDraw(canvas: Canvas?) {
 
         //Draw a black circle in the middle of the screen
