5c5f1ee1b23b4f954397dbf9b5fcbcacc6e00688
Patrick Chow <PatrickChow0803@gmail.com>
2019-08-22 16:21:00 -0400

    Draw a black circle in the middle.

--- app/src/main/java/com/patrickchow/androidcustomview/view/CircleCustomView.kt
+++ app/src/main/java/com/patrickchow/androidcustomview/view/CircleCustomView.kt
@@ -1,6 +1,8 @@
 package com.patrickchow.androidcustomview.view
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
@@ -8,5 +10,14 @@ import android.view.View
 class CircleCustomView (context: Context?, attrs: AttributeSet?): View(context, attrs){
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas: Canvas?) {
 
+        //Draw a black circle in the middle of the screen
+        paint.color = Color.BLACK
+        canvas?.drawCircle(width/2.0f, height/2.0f, 500f, paint)
+
+        
+
+        super.onDraw(canvas)
+    }
 }
